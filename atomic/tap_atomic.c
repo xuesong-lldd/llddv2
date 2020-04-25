9ff48daf1230c7e6ffee9e6a6f694b4e4988a430
xuesong chen <xuesong.cxs@outlook.com>
2020-04-25 08:40:11 +0800

    Add a upstream patch
    Signed-off-by: xuesong chen <xuesong.cxs@outlook.com>

--- atomic/tap_atomic.c
+++ atomic/tap_atomic.c
@@ -1,18 +1,24 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/spinlock.h>
+#include <linux/slab.h>
 
 static atomic_t g_lck_val = ATOMIC_INIT(1);
 
 static int tap_atomic_init(void)
 {
 	int old;
+	void *p;
+	unsigned long pa;
 	printk("++%s++\n", __func__);
+	p = vmalloc(64);
+	pa = __pa_symbol(alloc_workqueue);
+	printk("p = %p, pa = 0x%lx\n", p, pa);
 	old = atomic_cmpxchg(&g_lck_val, 1, 5);
 	printk("old = %d, new_lck_val = %d\n", old, atomic_read(&g_lck_val));
 	old = atomic_cmpxchg(&g_lck_val, 5, 3);
 	printk("old = %d, new_lck_val = %d\n", old, atomic_read(&g_lck_val));
-		
+	vfree(p);		
 	return 0;
 }
 
