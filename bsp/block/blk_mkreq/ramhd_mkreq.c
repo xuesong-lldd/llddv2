01654bdb4acdf646942032ea24091a012c74d402
xuesong chen <xuesong.cxs@outlook.com>
2020-08-25 22:26:07 +0800

    Update the 'ramhd_req' block device driver to DT-based
    So the updated driver can be bound to the 'ramdisk' platform
    device successfully, the 'ramdisk' device is added into the system
    via the device tree (dtb)
    
    Signed-off-by: xuesong chen <xuesong.cxs@outlook.com>

---
+++ bsp/block/blk_mkreq/ramhd_mkreq.c
@@ -0,0 +1,212 @@
+/*
+ * A very simple ram-based block device:
+ * Usage:
+ * 1. fdisk /dev/ramhda
+ * 2. mkfs.ext4 /dev/ramhda1
+ * 3. mount /dev/ramhda1 /mnt
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/fcntl.h>
+#include <linux/vmalloc.h>
+#include <linux/blkdev.h>
+#include <linux/hdreg.h>
+#include <linux/blk_types.h>
+
+#define RAMHD_NAME              "ramhd"
+#define RAMHD_MAX_DEVICE        2
+#define RAMHD_MAX_PARTITIONS    4
+
+#define RAMHD_SECTOR_SIZE       512
+#define RAMHD_SECTORS           32
+#define RAMHD_HEADS             4
+#define RAMHD_CYLINDERS         512
+
+#define RAMHD_SECTOR_TOTAL      (RAMHD_SECTORS * RAMHD_HEADS * RAMHD_CYLINDERS)
+#define RAMHD_SIZE              (RAMHD_SECTOR_SIZE * RAMHD_SECTOR_TOTAL) /* 32MB */
+
+typedef struct{
+	unsigned char *data;
+	struct request_queue *queue;
+	struct gendisk  *gd;
+}RAMHD_DEV;
+
+static char *sdisk[RAMHD_MAX_DEVICE] = {NULL,};
+static RAMHD_DEV *rdev[RAMHD_MAX_DEVICE] = {NULL,};
+
+static dev_t ramhd_major;
+
+static int ramhd_space_init(void)
+{
+	int i;
+	int err = 0;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		sdisk[i] = vmalloc(RAMHD_SIZE);
+		if(!sdisk[i]){
+			printk("vmalloc failed!");
+			err = -ENOMEM;
+			return err;
+		}
+		memset(sdisk[i], 0, RAMHD_SIZE);
+	}
+
+	return err;
+}
+
+static void ramhd_space_clean(void)
+{
+	int i;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		vfree(sdisk[i]);
+	}
+}
+
+static int ramhd_open(struct block_device *bdev, fmode_t mode)
+{
+	return 0;
+}
+
+static int ramhd_ioctl(struct block_device *bdev, fmode_t mode, unsigned cmd, unsigned long arg)
+{
+	struct hd_geometry geo;
+
+	switch(cmd)
+	{
+		case HDIO_GETGEO:
+			geo.cylinders = RAMHD_CYLINDERS;
+			geo.heads = RAMHD_HEADS;
+			geo.sectors = RAMHD_SECTORS;
+			geo.start = get_start_sect(bdev);
+			if(copy_to_user((void *)arg, &geo, sizeof(geo)))
+				return -EFAULT;
+			return 0;
+	}
+
+	return -ENOTTY;
+}
+
+static struct block_device_operations ramhd_fops =
+{
+	.owner = THIS_MODULE,
+	.open = ramhd_open,
+	.ioctl = ramhd_ioctl,
+};
+
+static blk_qc_t ramhd_make_request(struct request_queue *q, struct bio *bio)
+{
+	char *pRHdata;
+	void *pBuffer;
+	struct bio_vec bvec;
+	struct bvec_iter iter;
+
+	RAMHD_DEV *pdev = bio->bi_disk->private_data;
+	if (bio_end_sector(bio) > get_capacity(bio->bi_disk))
+		goto io_error;
+
+	pRHdata = pdev->data + (bio->bi_iter.bi_sector * RAMHD_SECTOR_SIZE);
+
+	bio_for_each_segment(bvec, bio, iter) {
+		pBuffer = kmap_atomic(bvec.bv_page) + bvec.bv_offset;
+		switch(bio_data_dir(bio))
+		{
+			case READ:
+				memcpy(pBuffer, pRHdata, bvec.bv_len);
+				flush_dcache_page(bvec.bv_page);
+				break;
+			case WRITE:
+				flush_dcache_page(bvec.bv_page);
+				memcpy(pRHdata, pBuffer, bvec.bv_len);
+				break;
+			default:
+				kunmap_atomic(pBuffer);
+				goto io_error;
+		}
+		kunmap_atomic(pBuffer);
+		pRHdata += bvec.bv_len;
+	}
+
+	bio_endio(bio);
+	return BLK_QC_T_NONE;
+io_error:
+	bio_io_error(bio);
+	return BLK_QC_T_NONE;
+}
+
+static int alloc_ramdev(void)
+{
+	int i;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		rdev[i] = kzalloc(sizeof(RAMHD_DEV), GFP_KERNEL);
+		if(!rdev[i])
+			return -ENOMEM;
+	}
+	return 0;
+}
+
+static void clean_ramdev(void)
+{
+	int i;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		if(rdev[i])
+			kfree(rdev[i]);
+	}   
+}       
+
+static int __init ramhd_init(void)
+{
+	int i;
+	int err;
+
+	err = ramhd_space_init();
+	if(err)
+		return err;
+	alloc_ramdev();
+
+	ramhd_major = register_blkdev(0, RAMHD_NAME);
+
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++)
+	{  
+		rdev[i]->data = sdisk[i];
+		rdev[i]->queue = blk_alloc_queue(GFP_KERNEL);
+		blk_queue_make_request(rdev[i]->queue, ramhd_make_request);
+		rdev[i]->gd = alloc_disk(RAMHD_MAX_PARTITIONS);
+		rdev[i]->gd->major = ramhd_major;
+		rdev[i]->gd->first_minor = i * RAMHD_MAX_PARTITIONS;
+		rdev[i]->gd->fops = &ramhd_fops;
+		rdev[i]->gd->queue = rdev[i]->queue;
+		rdev[i]->gd->private_data = rdev[i];
+		sprintf(rdev[i]->gd->disk_name, "ramhd%c", 'a'+i);
+		rdev[i]->gd->flags |= GENHD_FL_SUPPRESS_PARTITION_INFO;
+		set_capacity(rdev[i]->gd, RAMHD_SECTOR_TOTAL);
+		add_disk(rdev[i]->gd);
+	}
+	return 0;
+}
+
+static void __exit ramhd_exit(void)
+{
+	int i;
+
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++)
+	{
+		del_gendisk(rdev[i]->gd);
+		put_disk(rdev[i]->gd);
+		blk_cleanup_queue(rdev[i]->queue);  
+	} 
+	clean_ramdev();
+	ramhd_space_clean();
+	unregister_blkdev(ramhd_major,RAMHD_NAME);    
+}
+
+module_init(ramhd_init);
+module_exit(ramhd_exit);
+
+MODULE_AUTHOR("dennis chen @AMDLinuxFGL");
+MODULE_DESCRIPTION("The ramdisk implementation with request function");
+MODULE_LICENSE("GPL");
+
---
+++ bsp/block/blk_req/ramhd_req.c
@@ -0,0 +1,271 @@
+/*
+ * #insmod ramhd_req.ko
+ * #fdisk /dev/ramsda
+ * #mkfs.ext4 /dev/ramsda1
+ * #mount /dev/ramsda1 /mnt
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/fcntl.h>
+#include <linux/vmalloc.h>
+#include <linux/blkdev.h>
+#include <linux/hdreg.h>
+#include <linux/blk-mq.h>
+#include <linux/of_device.h>
+
+#define RAMHD_NAME              "ramsd"
+#define RAMHD_MAX_DEVICE        1
+#define RAMHD_MAX_PARTITIONS    2
+
+#define RAMHD_SECTOR_SIZE       512
+
+#define RAMHD_SECTORS           64
+#define RAMHD_HEADS             8
+#define RAMHD_CYLINDERS         512
+
+#define RAMHD_SECTOR_TOTAL      (RAMHD_SECTORS * RAMHD_HEADS * RAMHD_CYLINDERS)
+#define RAMHD_SIZE              (RAMHD_SECTOR_SIZE * RAMHD_SECTOR_TOTAL) /* 128-MB */
+
+
+typedef struct{
+	char   *data;
+	struct request_queue *queue;
+	spinlock_t      lock;
+	struct gendisk  *gd;
+}RAMHD_DEV;
+
+static char *sdisk[RAMHD_MAX_DEVICE];
+static RAMHD_DEV *rdev[RAMHD_MAX_DEVICE];
+
+static dev_t ramhd_major;
+static void ramhd_space_clean(void);
+
+struct ramhd_of_plt_data {
+	u32 nr_sectors;
+	u32 nr_heads;
+	u32 nr_cylinders;
+	u32 capacity;
+};
+
+static const struct ramhd_of_plt_data bcm2711_ramdisk_data = {
+	.nr_sectors = 64,
+	.nr_heads = 8,
+	.nr_cylinders = 512,
+	.capacity = 134217728 /* 128-MB */
+};
+
+static const struct of_device_id bcm2711_ramdisk_of_match[] = {
+	{ .compatible = "brcm,fake-ramdisk-dev", .data = &bcm2711_ramdisk_data },
+	{ .compatible = "arm, armv8-ramdisk" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, bcm2711_ramdisk_of_match);
+
+static int ramhd_space_init(void)
+{
+	int i;
+	int err = 0;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		sdisk[i] = vmalloc(RAMHD_SIZE);
+		if(!sdisk[i]){
+			err = -ENOMEM;
+			goto err_out;
+		}
+		memset(sdisk[i], 0, RAMHD_SIZE);
+	}
+
+	return err;
+
+err_out:
+	ramhd_space_clean();
+	return err;
+}
+
+static void ramhd_space_clean(void)
+{
+	int i;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		if (sdisk[i])
+			vfree(sdisk[i]);
+	}
+}
+
+static void clean_ramdev(void)
+{
+	int i;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		if(rdev[i])
+			kfree(rdev[i]);
+	}
+}
+
+
+static int alloc_ramdev(void)
+{
+	int i;
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++){
+		rdev[i] = kzalloc(sizeof(RAMHD_DEV), GFP_KERNEL);
+		if(!rdev[i])
+			goto err_out;
+	}
+	return 0;
+
+err_out:
+	clean_ramdev();
+	return -ENOMEM;
+}
+
+int ramhd_open(struct block_device *bdev, fmode_t mode)
+{
+	return 0;
+}
+
+static struct block_device_operations ramhd_fops =
+{   
+	.owner = THIS_MODULE,
+	.open = ramhd_open,
+};
+
+static blk_status_t ramhd_req_func (struct blk_mq_hw_ctx *hctx,
+				const struct blk_mq_queue_data *bd)
+{
+/*
+	struct request *req;
+	RAMHD_DEV *pdev;
+	char *pData;
+	unsigned long addr, size, start;
+	req = bd->rq;
+	while (req) {
+		start = blk_rq_pos(req); // The sector cursor of the current request
+		pdev = (RAMHD_DEV *)req->rq_disk->private_data;
+		pData = pdev->data;
+		addr = (unsigned long)pData + start * RAMHD_SECTOR_SIZE;
+		size = blk_rq_cur_bytes(req);
+		if (rq_data_dir(req) == READ)
+			memcpy(req->buffer, (char *)addr, size);
+		else
+			memcpy((char *)addr, req->buffer, size);
+
+		if(!__blk_end_request_cur(req, 0))
+			req = blk_fetch_request(q);
+	}
+*/
+	RAMHD_DEV *pdev;
+	struct bio_vec bv;
+	struct req_iterator iter;
+	char *addr, *pData, *buffer;
+	struct bio *bio;
+	struct request *req = bd->rq;
+	unsigned long start = blk_rq_pos(req); /* The start sector of the current request */
+	unsigned int size;
+	
+	pdev = (RAMHD_DEV *)req->rq_disk->private_data;
+	pData = pdev->data;
+
+	addr = pData + start * RAMHD_SECTOR_SIZE;
+	printk("Total sectors of current req:%d\n", blk_rq_sectors(req));
+	blk_mq_start_request(req);
+	/*
+	 * Iterates each segment of the current request. the bios maybe merged by the I/O
+	 * scheduler for the perf purpose, so the sectors are contiguous for the req after
+	 * merged, but the backing pages of the bios is unnecessary contiguous, so we need
+	 * to take care of each segment within the bio from current req...
+         */
+	rq_for_each_segment(bv, req, iter) {
+		bio = iter.bio;
+		size = bv.bv_len;
+		buffer = page_address(bv.bv_page) + bv.bv_offset;
+		printk("current seg[sector:%lld] buffer: 0x%px, size = %u@offset %u\n", bio->bi_iter.bi_sector, buffer, size, bv.bv_offset);
+		if ((unsigned long)buffer % RAMHD_SECTOR_SIZE) {
+			pr_err(RAMHD_NAME ": buffer %p not aligned\n", buffer);
+			return BLK_STS_IOERR;
+		}
+		spin_lock_irq(&pdev->lock);
+		if (rq_data_dir(req) == READ)
+			memcpy(buffer, addr, size);
+		else
+			memcpy(addr, buffer, size);
+		spin_unlock_irq(&pdev->lock);
+		addr += size;
+	}
+	blk_mq_end_request(req, BLK_STS_OK);
+	return BLK_STS_OK;
+}
+
+static struct blk_mq_tag_set tag_set[RAMHD_MAX_DEVICE];
+static const struct blk_mq_ops rdev_mq_ops = {
+	.queue_rq = ramhd_req_func,
+};
+
+int ramhd_init(void)
+{
+	int i;
+	ramhd_space_init();
+	alloc_ramdev();
+
+	ramhd_major = register_blkdev(0, RAMHD_NAME);
+
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++)
+	{
+		rdev[i]->data = sdisk[i]; 
+		rdev[i]->gd = alloc_disk(RAMHD_MAX_PARTITIONS);
+		spin_lock_init(&rdev[i]->lock);
+		rdev[i]->queue = blk_mq_init_sq_queue(&tag_set[i], &rdev_mq_ops, 2,
+					BLK_MQ_F_SHOULD_MERGE);;
+		rdev[i]->gd->major = ramhd_major;
+		rdev[i]->gd->first_minor = i * RAMHD_MAX_PARTITIONS;
+		rdev[i]->gd->fops = &ramhd_fops;
+		rdev[i]->gd->queue = rdev[i]->queue;
+		rdev[i]->gd->private_data = rdev[i];
+		sprintf(rdev[i]->gd->disk_name, "ramsd%c", 'a' + i);
+		set_capacity(rdev[i]->gd, RAMHD_SECTOR_TOTAL);
+		add_disk(rdev[i]->gd);
+	}
+
+	return 0;
+}
+
+static int ramhd_probe(struct platform_device *pdev)
+{
+	pr_info("+%s+\n", __func__);
+	return ramhd_init();
+}
+
+int ramhd_drv_unregister(struct platform_device *pdev)
+{
+	int i;
+	pr_info("+%s+\n", __func__);
+	for(i = 0; i < RAMHD_MAX_DEVICE; i++)
+	{
+		del_gendisk(rdev[i]->gd);
+		put_disk(rdev[i]->gd);     
+		blk_cleanup_queue(rdev[i]->queue);  
+		blk_mq_free_tag_set(&tag_set[i]);
+	}
+	unregister_blkdev(ramhd_major,RAMHD_NAME);  
+	clean_ramdev();
+	ramhd_space_clean();  
+
+	return 0;
+}
+
+static struct platform_driver ramhd_of_driver = {
+	.driver = {
+		.name = "ramsda",
+		.of_match_table = bcm2711_ramdisk_of_match,
+	},
+	.probe = ramhd_probe,
+	.remove = ramhd_drv_unregister,
+};
+
+module_platform_driver(ramhd_of_driver);
+
+MODULE_AUTHOR("dennis chen @ AMDLinuxFGL");
+MODULE_DESCRIPTION("update the driver to be compatible with dt-base one");
+MODULE_LICENSE("GPL");
+
